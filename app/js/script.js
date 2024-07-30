6aac89ddc9960281cadd1ea83acfca3b43910307
Sonja Krafft <sonmikrafft@gmail.com>
2024-07-30 22:26:26 +0200

    implement landing page

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,7 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".fade");
 
 /*
 * Helper Functions
@@ -12,3 +16,32 @@
 /*
 * Event Listeners
 * */
+btnHamburger.addEventListener("click", function () {
+    if (header.classList.contains("open")) {
+        body.classList.remove("noscroll");
+        header.classList.remove("open");
+        fadeElems.forEach(function (elem) {
+            elem.classList.add("fade-out");
+            elem.classList.remove("fade-in");
+        });
+    } else {
+        body.classList.add("noscroll");
+        header.classList.add("open");
+        fadeElems.forEach(function (elem) {
+            elem.classList.add("fade-in");
+            elem.classList.remove("fade-out");
+        });
+    }
+});
+
+/*
+Reset Width-dependent Elements
+ */
+window.addEventListener('resize', function() {
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    header.classList.remove("open");
+    fadeElems.forEach(function (elem) {
+        elem.classList.add("fade-out");
+        elem.classList.remove("fade-in");
+    });
+});
